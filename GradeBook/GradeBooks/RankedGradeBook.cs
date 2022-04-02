acc8f283053d7cfda0122aff92e4d47f6bf6240f
Patryk Domagała <Patryk.Domagala@microsoft.wsei.edu.pl>
2022-04-02 22:03:13 +0200

    13/24

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -40,6 +40,7 @@ namespace GradeBook.GradeBooks
                 return 'F';
             }
         }
+
         public override void CalculateStatistics()
         {
             if (Students.Count < 5)
@@ -50,5 +51,14 @@ namespace GradeBook.GradeBooks
             base.CalculateStatistics();
         }
 
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+            base.CalculateStudentStatistics(name);
+        }
     }
 }
